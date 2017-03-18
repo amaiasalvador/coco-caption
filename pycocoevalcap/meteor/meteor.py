3fe575ca1236058dc20fae1f74576ba5880d3086
amaiasalvador <amaia.salvador@upc.edu>
2017-03-18 12:52:05 +0100

    revert to python 2.7

--- pycocoevalcap/meteor/meteor.py
+++ pycocoevalcap/meteor/meteor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Python wrapper for METEOR implementation, by Xinlei Chen
-# Acknowledge Michael Denkowski for the generous discussion and help
+# Acknowledge Michael Denkowski for the generous discussion and help 
 
 import os
 import sys
@@ -24,7 +24,6 @@ class Meteor:
                 stderr=subprocess.PIPE)
         # Used to guarantee thread safety
         self.lock = threading.Lock()
-        print (self.meteor_p,type(self.meteor_p))
 
     def compute_score(self, gts, res):
         assert(gts.keys() == res.keys())
@@ -53,9 +52,7 @@ class Meteor:
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
         score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        write_line = '{}\n'.format(score_line)
-        write_line = bytes(write_line, 'UTF-8')
-        self.meteor_p.stdin.write(write_line)
+        self.meteor_p.stdin.write('{}\n'.format(score_line))
         return self.meteor_p.stdout.readline().strip()
 
     def _score(self, hypothesis_str, reference_list):
@@ -66,7 +63,7 @@ class Meteor:
         self.meteor_p.stdin.write('{}\n'.format(score_line))
         stats = self.meteor_p.stdout.readline().strip()
         eval_line = 'EVAL ||| {}'.format(stats)
-        # EVAL ||| stats
+        # EVAL ||| stats 
         self.meteor_p.stdin.write('{}\n'.format(eval_line))
         score = float(self.meteor_p.stdout.readline().strip())
         # bug fix: there are two values returned by the jar file, one average, and one all, so do it twice
@@ -74,7 +71,7 @@ class Meteor:
         score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
         return score
-
+ 
     def __del__(self):
         self.lock.acquire()
         self.meteor_p.stdin.close()
