8aa2487aa0927c88816fbc8de585fdb1f0056ca8
amaiasalvador <amaia.salvador@upc.edu>
2017-03-21 13:33:54 +0100

    default results file

--- eval_caps.py
+++ eval_caps.py
@@ -6,13 +6,13 @@ def get_parser():
 
     parser = argparse.ArgumentParser(description='Eval caption',epilog='The end.')
     parser.add_argument('-results_file', dest='results_file',
-                        default = './results/captions_val2014_fakecap_results.json',
+                        default = '/work/asalvador/sat_keras/results/model_gencaps.json',
                         help='Base name to save model')
     parser.add_argument('-ann_file', dest='ann_file',
                         default = '/seq/segmentation/COCO/tools/annotations/captions_val2014.json',
                         help='path to model file to load.')
     return parser
-    
+
 parser = get_parser()
 args_dict = parser.parse_args()
 
